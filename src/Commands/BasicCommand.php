34d9b836713093dfff16b2e67ccb102117e9fdf8
Doug Niccum <doug.niccum@schierproducts.com>
2025-12-16 20:54:43 -0600

    Add `BasicCommand` abstract class with environment setup logic.

---
+++ src/Commands/BasicCommand.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Dniccum\Vaultr\Commands;
+
+use Dniccum\Vaultr\Exceptions\InvalidEnvironmentConfiguration;
+use Illuminate\Console\Command;
+use function Laravel\Prompts\text;
+
+abstract class BasicCommand extends Command
+{
+    protected string $applicationId;
+
+    protected string $environmentSlug;
+
+    /**
+     * @throws \Throwable
+     */
+    protected function setEnvironment(): void
+    {
+        $this->applicationId = $this->option('application') ?? config('vaultr.default_application_id', '');
+        $this->environmentSlug = $this->option('environment') ?? config('app.env');
+
+        throw_if(empty($this->applicationId), new InvalidEnvironmentConfiguration('An application ID must be provided.'));
+        while (empty($this->environmentSlug)) {
+            $this->environmentSlug = text('The environment that you would like to interact with', required: true);
+        }
+    }
+}
