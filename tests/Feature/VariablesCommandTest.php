c691e082db17c44f8f8896305c3073fe15258523
Doug Niccum <dniccumdesign@gmail.com>
2026-01-21 10:32:30 -0600

    Refactor `VariablesCommandTest`: adjust test response structure and fix assertion format inconsistency

--- tests/Feature/VariablesCommandTest.php
+++ tests/Feature/VariablesCommandTest.php
@@ -85,18 +85,20 @@ it('correctly decrypts values during pull if key is provided', function () {
         $mock->shouldReceive('getVariables')
             ->once()
             ->andReturn([
-                [
-                    'id' => '019bd30d-ca8a-7241-9499-b9e7e8d4fbf4',
-                    'name' => 'MAIL_FROM_ADDRESS',
-                    'payload' => [
-                        'v' => 1,
-                        'alg' => 'AES-GCM',
-                        'kdf' => 'none',
-                        'iter' => 0,
-                        'salt' => 'OYnuBSYsDA-e4PYmBTRrOg',
-                        'iv' => '7v_mnKaw9ebxHcGb',
-                        'tag' => 'MmcWCpIdG9WFqS-pFFSBPQ',
-                        'ct' => 'ghWFuq4fOxtCb1QTPasg8m57a_J2N8TrpBKt',
+                'data' => [
+                    [
+                        'id' => '019bd30d-ca8a-7241-9499-b9e7e8d4fbf4',
+                        'name' => 'MAIL_FROM_ADDRESS',
+                        'payload' => [
+                            'v' => 1,
+                            'alg' => 'AES-GCM',
+                            'kdf' => 'none',
+                            'iter' => 0,
+                            'salt' => 'OYnuBSYsDA-e4PYmBTRrOg',
+                            'iv' => '7v_mnKaw9ebxHcGb',
+                            'tag' => 'MmcWCpIdG9WFqS-pFFSBPQ',
+                            'ct' => 'ghWFuq4fOxtCb1QTPasg8m57a_J2N8TrpBKt',
+                        ],
                     ],
                 ],
             ]);
@@ -108,7 +110,7 @@ it('correctly decrypts values during pull if key is provided', function () {
         ->assertSuccessful();
 
     $content = File::get($tempEnv);
-    expect($content)->toContain('MAIL_FROM_ADDRESS="hello@landworksstudio.com"');
+    expect($content)->toContain('MAIL_FROM_ADDRESS=hello@landworksstudio.com');
 
     unlink($tempEnv);
 });
