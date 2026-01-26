2cfb6745b165fd126756d2ff3e622a5d9e8977f0
Doug Niccum <dniccumdesign@gmail.com>
2026-01-25 21:49:47 -0600

    Add `VaultrShareCommand` to enable sharing environment access with team members

--- src/VaultrServiceProvider.php
+++ src/VaultrServiceProvider.php
@@ -5,6 +5,7 @@ namespace Dniccum\Vaultr;
 use Dniccum\Vaultr\Commands\VaultrEnvironmentsCommand;
 use Dniccum\Vaultr\Commands\VaultrInstallCommand;
 use Dniccum\Vaultr\Commands\VaultrKeysCommand;
+use Dniccum\Vaultr\Commands\VaultrShareCommand;
 use Dniccum\Vaultr\Commands\VaultrVariablesCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
@@ -13,11 +14,6 @@ class VaultrServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
         $package
             ->name('vaultr')
             ->hasConfigFile('vaultr')
@@ -26,6 +22,7 @@ class VaultrServiceProvider extends PackageServiceProvider
                 VaultrInstallCommand::class,
                 VaultrKeysCommand::class,
                 VaultrVariablesCommand::class,
+                VaultrShareCommand::class,
             ]);
     }
 }
