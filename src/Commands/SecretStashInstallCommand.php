7d683282923fa870bff34fa5ff670583339749cb
Doug Niccum <dniccumdesign@gmail.com>
2026-01-31 16:22:44 -0600

    Add `SecretStashInstallCommand` for initializing SecretStash via CLI

---
+++ src/Commands/SecretStashInstallCommand.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Dniccum\SecretStash\Commands;
+
+use function Laravel\Prompts\confirm;
+use function Laravel\Prompts\info;
+
+class SecretStashInstallCommand extends BasicCommand
+{
+    /**
+     * @var string
+     */
+    protected $signature = 'secret-stash:install';
+
+    /**
+     * @var string
+     */
+    protected $description = 'Install the SecretStash CLI';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(): int
+    {
+        if (confirm(
+            label: 'Would you like to publish the SecretStash config file?',
+        )) {
+            $this->call('vendor:publish', [
+                '--tag' => 'secret-stash-config',
+            ]);
+        }
+
+        $this->setEnvironment();
+
+        $this->call('secret-stash:keys', [
+            'action' => 'init',
+        ]);
+
+        info('SecretStash has been successfully initialized!');
+
+        return 0;
+    }
+}
